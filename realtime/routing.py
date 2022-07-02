b18da9b8711568960091d1fb5d84ac206b4de497
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-02 19:15:51 +0430

    ws url change

--- realtime/routing.py
+++ realtime/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/fx/signals', consumers.SignalsAlertWS.as_asgi()),
-    re_path(r'ws/fx/articles', consumers.ArticlesWS.as_asgi()),
+    re_path(r'ws/signals', consumers.SignalsAlertWS.as_asgi()),
+    re_path(r'ws/articles', consumers.ArticlesWS.as_asgi()),
 ]
