1efa787872e559fc5656ced27d088161cf5a5c1c
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-02 18:56:03 +0430

    update room temp

--- realtime/routing.py
+++ realtime/routing.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/signalsAlert', consumers.SignalsAlertWS.as_asgi()),
-    re_path(r'ws/articles', consumers.ArticlesWS.as_asgi()),
+    re_path(r'ws/fx/signalsAlert', consumers.SignalsAlertWS.as_asgi()),
+    re_path(r'ws/fx/articles', consumers.ArticlesWS.as_asgi()),
 ]
