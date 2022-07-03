75ec1e38f79b143837f83f34e19d7d35bff3f1d3
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-03 14:45:49 +0430

    license serializer

--- appsetting/admin.py
+++ appsetting/admin.py
@@ -12,9 +12,9 @@ class AppSettingAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=...):
         return False
 
-    list_display = ['is_active_trial', 'edit']
+    list_display = ['is_active_trial', 'trial_license', 'edit']
     list_display_links = ['edit']
-    list_editable = ['is_active_trial', ]
+    list_editable = ['is_active_trial', 'trial_license', ]
 
 
 admin.site.register(AppSetting, AppSettingAdmin)
