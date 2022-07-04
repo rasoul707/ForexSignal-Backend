79615e85f304777d35dd6c7bae53d2963317b1fd
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-05 03:30:25 +0430

    ssl again

--- api/settings.py
+++ api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'django_filters',
-    'sslserver',
+
 
     'rest_framework',
     'rest_framework.authtoken',
@@ -256,3 +256,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://therealrole.com:2053",
     "https://therealrole.com:2053",
 ]
+
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
