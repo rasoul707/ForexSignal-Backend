92d6698376c49ed3f793ef81ed7cdb02701d82e5
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-04 20:28:33 +0430

    add default settinga

---
+++ appsetting/migrations/0002_auto_20220704_2027.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+
+def addDefault(apps, schema_editor):
+    AppSetting = apps.get_model('appsetting', 'AppSetting')
+    setting = AppSetting()
+    setting.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('appsetting', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(addDefault),
+    ]
