dd39274d562f7a4bd5039a797e16fa891b63ff62
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-05 21:59:31 +0430

    paging

--- mynews/views.py
+++ mynews/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from .models import *
 from .serializers import *
 from rest_framework import permissions, viewsets, response
+from django.core.paginator import Paginator
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_query_param = 'p'
 
 # Create your views here.
 
 
 class MyNewsViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = MyNewsSerializer
-    queryset = MyNews.objects.all()
+    queryset = MyNews.objects.filter(status='publish')
+    pagination_class = StandardResultsSetPagination
