fc33808b947ec09506a7a58ac0dd9e3b1dde7e37
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-06 17:33:57 +0430

    add path new

--- notice/urls.py
+++ notice/urls.py
@@ -10,4 +10,4 @@ router.register(r'broker', BrokerViewSet)
 router.register(r'signal', SignalAlertViewSet)
 # router.register(r'new', NewSignal.as_view())
 
-urlpatterns = router.urls + path('new', NewSignal.as_view())
+urlpatterns = router.urls + [path('new', NewSignal.as_view())]
