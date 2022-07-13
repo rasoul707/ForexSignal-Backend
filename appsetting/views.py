f27f2dac360a9906ccac850c234de2630fd81467
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-07-13 05:09:32 +0430

    allow any

--- appsetting/views.py
+++ appsetting/views.py
@@ -1,11 +1,14 @@
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import *
 from .serializers import *
 from rest_framework.response import Response
+
+
 # Create your views here.
 
 
 class AppSettingViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AppSettingSerializer
     queryset = AppSetting.objects.all()
+    permission_classes = [permissions.AllowAny]
