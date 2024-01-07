ca3c5833e1c1e3c6606b150f9c38e348ac0b1f66
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-01-07 23:30:37 +0330

    fixed redis

--- api/settings.py
+++ api/settings.py
@@ -88,7 +88,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("tai.liara.cloud", 31082)],
         },
     },
 }
