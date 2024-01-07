426124b7817beeaaaee2fbfe3281a71e891008e6
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-01-07 12:01:17 +0330

    update database

--- api/settings.py
+++ api/settings.py
@@ -200,7 +200,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data/db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
