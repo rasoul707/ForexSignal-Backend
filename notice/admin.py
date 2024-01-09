0390cdc910fd1dfde197490c977a2186f217a2fb
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-01-09 11:58:23 +0330

    remove migrations and fixed signal alert animations

--- notice/admin.py
+++ notice/admin.py
@@ -3,5 +3,11 @@ from .models import *
 # Register your models here.
 
 
+class SignalAlertAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'broker', 'percent', 'winrate', 'is_active', 'description']
+    list_display_links = ['edit']
+    list_editable = ['percent', 'winrate', 'is_active']
+
 admin.site.register(Broker)
-admin.site.register(SignalAlert)
+admin.site.register(SignalAlert, SignalAlertAdmin)
